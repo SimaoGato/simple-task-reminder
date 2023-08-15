54c44661ab91b2034ba32412d277c7e44ca66abf
Simão Gato <simao.valedegato@gmail.com>
2023-08-15 09:49:02 +0100

    feat: Implement CLI Interface and Placeholder Functions
    Added the command-line argument parser and placeholder functions for adding, deleting, listing, updating, and searching tasks. This commit lays the foundation for the core functionality of the terminal-based task reminder system.

--- task_manager.py
+++ task_manager.py
@@ -2,6 +2,7 @@ import argparse
 
 class Task:
     def __init__(self, task_id, description, status, due_date, priority):
+        # Initialize a Task object with provided attributes
         self.task_id = task_id
         self.description = description
         self.status = status
@@ -9,6 +10,7 @@ class Task:
         self.priority = priority
 
 def create_parser():
+    # Create a parser for command-line arguments
     parser = argparse.ArgumentParser(description='Terminal-Based Task Reminder')
     parser.add_argument('-a', '--add', help='Add a new task', action='store_true')
     parser.add_argument('-d', '--delete', help='Delete a task', action='store_true')
@@ -17,10 +19,32 @@ def create_parser():
     parser.add_argument('-s', '--search', help='Search for a task', action='store_true')
     return parser
 
+def add_task():
+    # Function to add a new task
+    pass
+
+def delete_task():
+    # Function to delete a task
+    pass
+
+def list_tasks():
+    # Function to list all tasks
+    pass
+    
+def update_task():
+    # Function to update a task
+    pass
+
+def search_task():
+    # Function to search for a task
+    pass
+
 def main():
+    # Create the argument parser
     parser = create_parser()
     args = parser.parse_args()
 
+    # Based on command-line arguments, perform corresponding actions
     if args.add:
         print('Adding a new task')
     elif args.delete:
@@ -32,7 +56,10 @@ def main():
     elif args.search:
         print('Searching for a task')
     else:
+        # Print help message if no valid command-line arguments are provided
         parser.print_help()
 
 if __name__ == '__main__':
+    # Call the main function when the script is executed
     main()
+
