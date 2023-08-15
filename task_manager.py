804f4d8b86d82fd3f1f455adeb8fc4e66418a449
Simão Gato <simao.valedegato@gmail.com>
2023-08-15 09:43:42 +0100

    feat: Implement CLI Argument Handling
    Added functionality to handle different CLI arguments in the main function. The script now correctly processes commands for adding, deleting, listing, updating, and searching tasks based on user input.
    
    - Added logic to handle 'add', 'delete', 'list', 'update', and 'search' commands.
    - Display corresponding messages for each command.

--- task_manager.py
+++ task_manager.py
@@ -16,3 +16,23 @@ def create_parser():
     parser.add_argument('-u', '--update', help='Update a task', action='store_true')
     parser.add_argument('-s', '--search', help='Search for a task', action='store_true')
     return parser
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.add:
+        print('Adding a new task')
+    elif args.delete:
+        print('Deleting a task')
+    elif args.list:
+        print('Listing all tasks')
+    elif args.update:
+        print('Updating a task')
+    elif args.search:
+        print('Searching for a task')
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
