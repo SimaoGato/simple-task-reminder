f1eb8fb2e10f263e7ff19425b5527e1b5187b1eb
Simão Gato <simao.valedegato@gmail.com>
2023-08-15 09:38:24 +0100

    feat: Introduce CLI Interface
    Added the CLI argument parser and relevant code to enable command-line interaction. This commit introduces the foundation for user interaction with the terminal-based task reminder system.
    
    - Added argparse library for parsing command-line arguments.
    - Defined command-line options for adding, deleting, listing, updating, and searching tasks.

--- task_manager.py
+++ task_manager.py
@@ -7,3 +7,12 @@ class Task:
         self.status = status
         self.due_date = due_date
         self.priority = priority
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Terminal-Based Task Reminder')
+    parser.add_argument('-a', '--add', help='Add a new task', action='store_true')
+    parser.add_argument('-d', '--delete', help='Delete a task', action='store_true')
+    parser.add_argument('-l', '--list', help='List all tasks', action='store_true')
+    parser.add_argument('-u', '--update', help='Update a task', action='store_true')
+    parser.add_argument('-s', '--search', help='Search for a task', action='store_true')
+    return parser
